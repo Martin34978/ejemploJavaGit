e6f668d4ac19bbfa141f769a5c8f00dc76a2712d
Martin <martindaw@protonmail.com>
2022-05-09 17:13:30 +0200

    Primer Commit

---
+++ Banco/src/CuentaCorriente.java
@@ -0,0 +1,13 @@
+import javax.swing.border.StrokeBorder;
+
+public class CuentaCorriente {
+    String dni;
+    String nombre;
+    double saldo;
+
+    cuentaCorriente(String dni, String nombre){
+        this.dni = dni;
+        this.nombre = nombre;
+        
+    }
+}
---
+++ Banco/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        CuentaCorriente c;
+        c = new CuentaCorriente(dni:"23423433", nombre:"Pepe");
+    }
+}
